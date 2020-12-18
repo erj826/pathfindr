1a0248158e3db0f4fe000a0bc5fbe3fc6f06be36
Alex Caulfield <alex.caulfield28@gmail.com>
2020-12-18 14:45:00 -0500

    adding install hook

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ Load the `./build` directory as an unpacked extension.
 ### Based on this tutorial
 
 https://medium.com/@yuridrabik/create-chrome-devtools-extension-using-react-js-1b8e16e8dc72
+
+
+### Helpful links
+https://lihautan.com/personalised-development-workspace-with-chrome-extension
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,10 +1,10 @@
-setTimeout(() => {
-  if (
-    !!window.React ||
-    !!document.querySelector("[data-reactroot], [data-reactid]")
-  )
-    console.log("React.js");
-}, 5000);
+// setTimeout(() => {
+//   if (
+//     !!window.React ||
+//     !!document.querySelector("[data-reactroot], [data-reactid]")
+//   )
+//     console.log("React.js");
+// }, 5000);
 
 chrome.extension.onRequest.addListener(function (
   request,
@@ -15,3 +15,21 @@ chrome.extension.onRequest.addListener(function (
     sendResponse({ dom: document.querySelector("h2").innerHTML });
   else sendResponse({}); // Send nothing..
 });
+
+function installHook() {
+    if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== 'undefined') {
+      console.log('found react dev tools')
+      const reactInstances = window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers || null;
+      const instance = reactInstances.get(1);
+      // const reactRoot = window.document.body.childNodes;
+      const devTools = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
+      console.log(instance)
+    } else {
+      console.log('sadly didnt find react dev tools')
+    }
+}
+
+// execute the install hook in web page context
+const script = document.createElement('script');
+script.textContent = `;(${installHook.toString()})();`;
+document.documentElement.appendChild(script);
