7b6d9ccab03d368ab46a874952d8b86d567b1fad
Alex Caulfield <alex.caulfield28@gmail.com>
2020-12-18 16:10:26 -0500

    adding recursive call, need to confirm what value to traverse on

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -6,6 +6,43 @@ setTimeout(() => {
 
 function installHook() {
   let __ReactSightFiberDOM;
+
+  function createComponentRecursively(node, parents) {
+    console.log('in create recursive')
+    const newComponent = {
+      name: '',
+      children: [],
+      state: null,
+      props: null,
+      id: null,
+      isDOM: null,
+    };
+
+    console.log(node);
+
+    if (node.type) {
+      if (node.type.name) {
+        newComponent.name = node.type.name;
+        newComponent.isDOM = false;
+      }
+      else {
+        newComponent.name = node.type;
+        newComponent.isDOM = true;
+      }
+    }
+    newComponent.children = [];
+    console.log(newComponent);
+    parentArr.push(newComponent);
+    if (node.child != null) createComponentRecursively(node.child, newComponent.children);
+    if (node.sibling != null) createComponentRecursively(node.sibling, parents);
+  }
+
+  function componentTreeCreator(node) {
+    const components = [];
+    console.log(node);
+    // createComponentRecursively(node.current.stateNode.current)
+    // console.log(components)
+  }
   if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== 'undefined') {
     const devTools = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
     const reactInstances = window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers || null;
@@ -15,15 +52,10 @@ function installHook() {
     devTools.onCommitFiberRoot = (function (original) {
       return function (...args) {
         __ReactSightFiberDOM = args[1];
-        console.log('DOM: ', __ReactSightFiberDOM);
-        traverse16(__ReactSightFiberDOM);
+        componentTreeCreator(__ReactSightFiberDOM);
         return original(...args);
       };
     })(devTools.onCommitFiberRoot);
-  
-    // This will print like an infinite # of components to console.
-    console.log(devTools.onCommitFiberRoot());
-
   } else {
     console.log('sadly didnt find react dev tools')
   }
